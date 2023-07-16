66168ff206bbe48c4042cb32aa85acbda97130c2
sintumeng <sintumeng01@gmail.com>
2023-07-16 06:57:24 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,12 @@
  */
 int main (void)
 {
-            printf("with proper grammar, but the outcome is a piece of art,\n");
-            return (0);
+            printf("size of a char:  %lu byte (s)\n"; sizeof(char));
+	    printf("size of an int:  %lu byte (s)\n"; sizeof(int));
+	    printf("size of a long int: %lu byte (s)\n"; sizeof(longint));
+	    printf("size of a long long int: %lu byte (s)\n"; sizeof(long long int));
+	    printf("size of a float: %lu byte (s)\n"; sizeof(float));
+	    return (0);
+
+  
 }
