09b2c2f77dfb1e8c38d4237904117bd1db95421b
sintumeng <sintumeng01@gmail.com>
2023-07-17 01:07:56 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,7 @@
  * Return: always 0 (success)
  */
 int main (void)
-{	puts("\"Programming is like building a multilingual puzzle\n ");	
-	return  (0);
+{	puts("\"Programming is like building a multilingual puzzle\n");
+        return  (0);
+        
 }		
